93f056a3790df0630a727773affefedacc4c063e
Qwarning <108075046+Qwarning@users.noreply.github.com>
2023-05-06 19:52:30 +0900

    Auto Door Opening

---
+++ Casualty/Assets/Script/cshAutoDoorDetective.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cshAutoDoorDetective : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider col)
+    {
+        if(col.gameObject.tag == "AutoDoorDetectArea")
+        {
+            col.gameObject.GetComponent<cshOpenDoor>().OnOff = true;
+            print("true");
+        }
+    }
+    private void OnTriggerExit(Collider col)
+    {
+        if (col.gameObject.tag == "AutoDoorDetectArea")
+        {
+            col.gameObject.GetComponent<cshOpenDoor>().OnOff = false;
+            print("false");
+        }
+    }
+}
