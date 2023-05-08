48c79a9a9f693b93c2d08f080ed0c53233f5104e
rlathf815 <rlathfrlathf@gmail.com>
2023-05-09 02:41:31 +0900

    sol_0509 1st commit
    PatientData ScriptableObject 생성

--- Casualty/Assets/Script/ActiveBtn.cs
+++ Casualty/Assets/Script/ActiveBtn.cs
@@ -1,24 +1,37 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using System.Collections.Generic;
 
 public class ActiveBtn : MonoBehaviour
 {
     //public string nextSceneName;
     public GameObject UI;
-    
+    public PatientData selectedPatient;
+    public List<PatientData> allPatientData;
+
     void OnTriggerEnter(Collider other)
     {
         Debug.Log("trigger entered" + other.tag);
-        
+        Debug.Log("current patient name = "  + gameObject.name);
+
+
         if (other.gameObject.CompareTag("Player"))
         {
             UI.SetActive(true);
-            if (Input.GetKey(KeyCode.E))
+            //if (Input.GetKey(KeyCode.E))
+            //{
+            //    // Load the new scene
+            //    SceneManager.LoadScene("MiniGameScene");
+            //}
+            foreach (PatientData patient in allPatientData)
             {
-                // Load the new scene
-                SceneManager.LoadScene("MiniGameScene");
+                if (patient.patientName == gameObject.name)
+                {
+                    Debug.Log(patient.patientName + " found");
+                    selectedPatient.patientName = patient.patientName;
+                    selectedPatient.description.AddRange(patient.description);
+                }
             }
-
         }
 
     }
@@ -28,7 +41,7 @@ public class ActiveBtn : MonoBehaviour
         if (other.gameObject.CompareTag("Player"))
         {
             UI.SetActive(false);
-
+            selectedPatient.description.Clear();
         }
     }
 }
---
+++ Casualty/Assets/Script/PatientData/PatientData.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "New Patient Data", menuName = "Patient Data", order = 1)]
+public class PatientData : ScriptableObject
+{
+    public string patientName;
+    public List<string> description;
+    // Add any other relevant fields
+}
