5636ecba80b57dbdf9a9fe2e32f7b4bb7a2127a3
kimjoungyoo <94528532+kimjoungyoo@users.noreply.github.com>
2023-05-11 01:54:54 +0900

    lookAt.cs 변경
    x, z회전값은 고정하고
    LookAt함수의 y회전값만 사용하게 바꿈.

--- Casualty/Assets/Script/lookAt.cs
+++ Casualty/Assets/Script/lookAt.cs
@@ -5,11 +5,19 @@ using UnityEngine;
 public class lookAt : MonoBehaviour
 {
     public Transform player;
+    public Transform LookatPlayer;
     public Animator animator;
 
     void Update()
     {
         transform.LookAt(player);
+
+        // Vector3 currentRotation = transform.eulerAngles; // 객체 현 회전값
+
+        Quaternion newRotation = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+        //y값만 회전하고 나머지 (x는 앞뒤로 기울고, z는 왼쪽오른쪽으로 휙기움)는 고정.
+        transform.rotation = newRotation;//적용
+
         animator.SetFloat("LookWeight", 1f);
     }
 
