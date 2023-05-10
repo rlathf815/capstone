fe77f72ae4820b916b7178886628c8082992c844
Qwarning <108075046+Qwarning@users.noreply.github.com>
2023-05-10 17:22:58 +0900

    asd

---
+++ Casualty/Assets/Script/StitchgameClear.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StitchgameClear : MonoBehaviour
+{
+    public GameObject ui;
+    public GameObject gut;
+
+    void start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        if(gut == null)
+        {
+            ui.SetActive(true);
+     //  }
+     //  if (ui != null && ui.gameObject.activeSelf)
+     //  {
+            StartCoroutine(LoadMapSceneWithDelay());
+        }
+
+    }
+    IEnumerator LoadMapSceneWithDelay() //Coroutine 사용하여 씬 전환 전 잠시 딜레이
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        SceneManager.LoadScene("MapScene");
+    }
+}
--- Casualty/Assets/gameManager.cs
+++ Casualty/Assets/gameManager.cs
@@ -6,9 +6,34 @@ using UnityEngine.SceneManagement;
 public class gameManager : MonoBehaviour
 {
     public Canvas ui;
+    public GameObject surgeryUI;
+    public PatientData selectedPatient;
+    [SerializeField] public GameObject[] patients;
+    private GameObject patient;
 
+    void Start()
+    {
+        for (int i = 0; i < patients.Length; i++)
+        {
+            patients[i].SetActive(false);
+            if (patients[i].name == selectedPatient.patientName)
+                patient = patients[i];
+        }
+        Debug.Log("patient name = " + selectedPatient.patientName);
+        Debug.Log("patient found = " + patient);
+
+        patient.SetActive(true);
+    }
     void Update()
     {
+
+        if(patient.name == "patient2")
+        {
+            surgeryUI.SetActive(true);
+            StartCoroutine(LoadStitchSceneWithDelay());
+
+        }
+
         if (ui != null && ui.gameObject.activeSelf)
         {
             StartCoroutine(LoadMapSceneWithDelay());
@@ -20,5 +45,11 @@ public class gameManager : MonoBehaviour
         yield return new WaitForSeconds(2.0f); 
 
         SceneManager.LoadScene("MapScene"); 
+    }
+    IEnumerator LoadStitchSceneWithDelay() //Coroutine 사용하여 씬 전환 전 잠시 딜레이
+    {
+        yield return new WaitForSeconds(2.0f);
+
+        SceneManager.LoadScene("StitchMiniGameScene");
     }
 }
