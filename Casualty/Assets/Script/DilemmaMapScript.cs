9a58d6eca9d8d0939e990f433ca8d0a763d41e53
Cheeztabby <cheeztabby@gmail.com>
2023-05-19 02:47:10 +0900

    세팅 수정
    1

--- Casualty/Assets/Script/DilemmaMapScript.cs
+++ Casualty/Assets/Script/DilemmaMapScript.cs
@@ -5,9 +5,6 @@ using UnityEngine;
 public class DilemmaMapScript : MonoBehaviour
 {
     public GameObject Elevator;
-    public SharedData sharedData;//쉐어드 데이터.
-    public GameObject ghost;
-    public LookAtPlayer lookAtPlayer;
 
     //public Transform ElevatorPos;
     public GameObject Player;
@@ -24,56 +21,44 @@ public class DilemmaMapScript : MonoBehaviour
     void Start()
     {
         rb = Player.GetComponent<Rigidbody>();
-        lookAtPlayer = ghost.GetComponent<LookAtPlayer>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (lookAtPlayer.triggerGhost == true)
+        speed = rb.velocity.magnitude;
+        float distance = Mathf.Abs(transform.position.x - player.position.x);
+
+        /*
+        if (Player.transform.position.x == -15)
         {
-            speed = rb.velocity.magnitude;
-            float distance = Mathf.Abs(transform.position.x - player.position.x);
-
-            /*
-            if (Player.transform.position.x == -15)
+            
+        }
+        */
+        
+        if(transform.position.x >= -120)
+        {
+            if (distance < distanceToPlayer && isMoving)
             {
-
-            }
-            */
-
-            if (transform.position.x >= -120)
-            {
-                if (distance < distanceToPlayer && isMoving)
-                {
-                    // 일정 거리 이하로 가까워졌을 때, 멈추도록 합니다.
-                    isMoving = false;
-                }
-                else if (distance > distanceToPlayer && !isMoving)
-                {
-                    // 일정 거리 이상으로 멀어졌을 때, 다시 이동합니다.
-                    isMoving = true;
-                }
-            }
-            else if (transform.position.x < -120)
-            {
+                // 일정 거리 이하로 가까워졌을 때, 멈추도록 합니다.
                 isMoving = false;
             }
-
-
-            if (Player.transform.position.x < -134)
-            {//엘리베이터 안에 들어갔을 때
-
-                sharedData.dillemaRunOver = true;
-
-            }
-
-            if (isMoving)
+            else if (distance > distanceToPlayer && !isMoving)
             {
-                // 플레이어 쪽으로 이동합니다.
-                float direction = Mathf.Sign(player.position.x - transform.position.x);
-                transform.Translate(Vector3.right * direction * speed * Time.deltaTime);
+                // 일정 거리 이상으로 멀어졌을 때, 다시 이동합니다.
+                isMoving = true;
             }
+        }else if(transform.position.x < -120)
+        {
+            isMoving = false;
+        }
+       
+
+        if (isMoving)
+        {
+            // 플레이어 쪽으로 이동합니다.
+            float direction = Mathf.Sign(player.position.x - transform.position.x);
+            transform.Translate(Vector3.right * direction * speed * Time.deltaTime);
         }
     }
 }
--- Casualty/Assets/Script/LookAtPlayer.cs
+++ Casualty/Assets/Script/LookAtPlayer.cs
@@ -4,123 +4,75 @@ using UnityEngine;
 
 public class LookAtPlayer : MonoBehaviour
 {
-    public SharedData sharedData;//쉐어드 데이터.
-
     public Transform playerTransform;
     public float distance = 5.0f;
     public float speed = 5.0f;
-    public float minDistance = 1.25f;//죽는거리
-    public float maxDistance = 2.0f;//일정거리
+    public float minDistance = 0.5f;//얼마나 붙어올지
+    public float maxDistance = 1.0f;//일정거리
     public Vector3 velocity;
     private Animator animator;
     public float currentDistance;
-
-    public bool triggerGhost = false;//귀신이 작동하게되는 선
 
     private float timer;
 
     public AudioSource audioSource;
     public AudioClip audioClip;
-    public AudioClip Jumpscare_sound;
 
     public GameObject Player;
     private Rigidbody rb;
 
     private void Start()
     {
+
+
         animator = GetComponent<Animator>();
         animator.applyRootMotion = true;
-        sharedData.dillemaRunOver = false; // 딜레마씬 입장씨 한번 초기화
 
         rb = Player.GetComponent<Rigidbody>();
     }
 
     private void FixedUpdate()
     {
-        if(Player.transform.position.x > -24)
-        {
-            //audioSource.PlayOneShot(Jumpscare_sound); 소리가 겹쳐나서 보류
-            triggerGhost = true;
-            //sharedData.dillemaRunOver = true;
+        
+        // 플레이어 방향을 바라보도록 회전
+        Vector3 direction = playerTransform.position - transform.position;
+        direction.y = 0;//y 디렉션은 고정.
+        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), speed * Time.deltaTime);
 
-            //계단이 보이는곳까지가면 귀신 활성화
+        // 일정거리를 유지하며 플레이어를 따라 이동
+        currentDistance = Vector3.Distance(transform.position, playerTransform.position);
+        if(currentDistance > 12.0f)
+        {//12이상 멀어지면 갑작스럽게 달려나오면서 한번 가속함.
+            velocity = transform.forward * speed *1.2f;
+        }
+        else if (currentDistance <= 12.0f && currentDistance > maxDistance)
+        {
+            velocity = transform.forward * speed;
+        }
+        else if (currentDistance < minDistance)
+        {
+            velocity = -transform.forward * speed;
         }
 
-        if (triggerGhost)
+        if (transform.position.x <= -132.5f)
+        {//-133 x포지션이 귀신이 문앞에 도달하는건데, 가끔 문을 침범함. 고로 아예 멈추게 하였음.
+            //추가로 이 멈춘상태에서 적절한 애니메이션이 있는지도 찾아볼 예정임.
+            velocity = transform.forward * 0;
+        }
+
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        rigidbody.velocity = new Vector3(velocity.x, rigidbody.velocity.y, velocity.z);
+
+        if (timer <= 0.75f)
         {
-            // 플레이어 방향을 바라보도록 회전
-            Vector3 direction = playerTransform.position - transform.position;
-            direction.y = 0;//y 디렉션은 고정.
-            transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(direction), speed * Time.deltaTime);
+            timer += Time.deltaTime;
 
-            // 일정거리를 유지하며 플레이어를 따라 이동
-            currentDistance = Vector3.Distance(transform.position, playerTransform.position);
-            if (currentDistance > 12.0f)
-            {//12이상 멀어지면 갑작스럽게 달려나오면서 한번 가속함.
-                velocity = transform.forward * speed * 1.2f;
-            }
-            else if (currentDistance <= 12.0f && currentDistance > maxDistance)
-            {
-                velocity = transform.forward * speed;
-            }
-            else if (currentDistance < minDistance)
-            {
-                Debug.Log("close");
-                System.Reflection.FieldInfo field = sharedData.GetType().GetField("dillemaRunOver");
-                if (field != null && field.FieldType == typeof(bool))
-                {
-                    bool currentValue = (bool)field.GetValue(sharedData);
-                    if (!currentValue)
-                    {
-                        field.SetValue(sharedData, true);
-                        Debug.Log("Dead");
-                    }
-                }
-                // sharedData.dillemaRunOver = true;
-                velocity = -transform.forward * speed;
-            }
-
-            if (transform.position.x <= -132.5f)
-            {//-133 x포지션이 귀신이 문앞에 도달하는건데, 가끔 문을 침범함. 고로 아예 멈추게 하였음.
-             //추가로 이 멈춘상태에서 적절한 애니메이션이 있는지도 찾아볼 예정임.
-                velocity = transform.forward * 0;
-            }
-
-            Rigidbody rigidbody = GetComponent<Rigidbody>();
-            rigidbody.velocity = new Vector3(velocity.x, rigidbody.velocity.y, velocity.z);
-
-            if (timer <= 0.75f)
-            {
-                timer += Time.deltaTime;
-
-            }
-            else if (timer > 0.75f)
-            {
-                audioSource.PlayOneShot(audioClip);
-                timer = 0f;
-            }
+        }else if(timer > 0.75f)
+        {
+            audioSource.PlayOneShot(audioClip);
+            timer = 0f;
         }
     }
-
-    /*
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.gameObject.CompareTag("Player"))
-        {
-            System.Reflection.FieldInfo field = sharedData.GetType().GetField("dillemaRunOver");
-            if (field != null && field.FieldType == typeof(bool))
-            {
-                bool currentValue = (bool)field.GetValue(sharedData);
-                if (!currentValue)
-                {
-                    field.SetValue(sharedData, true);
-                    Debug.Log("Dead");
-                }
-            }//닿으면 데이터전달.
-            //아직 점프스케어 적용못함.
-        }
-    }
-    */
 
 
 }
