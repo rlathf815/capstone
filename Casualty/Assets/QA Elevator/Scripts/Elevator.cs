91e749fcca0645f1694d04a316c276e4490a0014
Qwarning <108075046+Qwarning@users.noreply.github.com>
2023-05-21 18:44:19 +0900

    쫓길 때 엘베에 다가가면 자동으로 문이 열리도록 수정, 발소리 3d음향 및 거리 비례 감소 적용

--- Casualty/Assets/QA Elevator/Scripts/Elevator.cs
+++ Casualty/Assets/QA Elevator/Scripts/Elevator.cs
@@ -3,7 +3,11 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Elevator : MonoBehaviour {
-	private bool inTrigger = false;
+    public GameObject Humanoid;
+    public LookAtPlayer lookAtPlayer;
+	//from Q
+
+    private bool inTrigger = false;
 	private Rigidbody Player;
 	private Transform PlayerCam;
 
@@ -93,7 +97,9 @@ public class Elevator : MonoBehaviour {
 
 	// Use this for initialization
 	void Awake () {
-		if (GetComponentInChildren<ReflectionProbe> ()) {
+        lookAtPlayer = Humanoid.GetComponent<LookAtPlayer>();//from Q
+
+        if (GetComponentInChildren<ReflectionProbe> ()) {
 			probe = GetComponentInChildren<ReflectionProbe> ();
 			probe.refreshMode = UnityEngine.Rendering.ReflectionProbeRefreshMode.OnAwake;
 			isReflectionProbe = true;
@@ -534,12 +540,10 @@ public class Elevator : MonoBehaviour {
 	}
 
 	void OnTriggerEnter(Collider other){
-		if(other.tag == "AutoDoorDetectArea")
+		if(other.tag == "AutoDoorDetectArea" && lookAtPlayer.triggerGhost == true)
 		{
 			DoorsOpening();
-		}
-
-
+		}//from Q
 
 		if(other.gameObject == Player.gameObject){
 			inTrigger = true;
