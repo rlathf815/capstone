84722e1ca893a3fc4cb9161c7c048bce77d795cf
Cheeztabby <cheeztabby@gmail.com>
2023-05-21 19:36:41 +0900

    버그픽스 재업로드 + 세팅값 변경
    1.attack 애니메이션 이상하게 나오던거 변경..
    Animation Controller에서 Trigger가 켜져있었어요
    
    2. 사운드, 애니메이션 이벤트 추가 (AIController.cs -> AudioSoruce 변수들로 제어)
    2-1) 배경음악: 디폴트 값으로 AI가 Player를 찾지 못하고 순회하거나 이동하는상태(Idle,Walking)일때 실행
    2-2) 추격: 비명 이후 추격사운드로 전환, 추격을 벗어나면 다시 배경음악으로 변경
    2-3) 비명 사운드: Player가 AI에게 적발시 사운드 실행, 한번만 실행되는게 자연스러움
    2-4) 발자국 사운드: Walking, Running 둘다 적용, 애니메이션 이벤트로 적용했으므로
                       walkSpeed, ChaseSpeed 값을 바꿔도 동기화 되어 적용됨. 발자국은 거리 비례 사운드로 변경
    + 사운드랑 애니메이션 길이 동기화
    
    3. 밝기 변경
    3-1) Spotlight 밝기 세부값 조정
    3-2) 손전등 칼라 변경
    3-3) Lighting-Environment-Intensity Multiplier 1.0-> 0.45
    
    4. Teleportation 추가
    순간이동 조건
    1. AI에게 잡혔을 때
    	플레이어가 AI에게 attack 당하면 일정 위치로 순간이동함
    	AI 객체에 Teleport Target Object 추가하면됨
    2. 트리거박스를 밟았을때
    	PlayerTeleportationToMogue 오브젝트 밟으면 영안실로 이동함
    	TeleportPlayer.cs에 값 줘야함

--- Casualty/Assets/Script/AIController.cs
+++ Casualty/Assets/Script/AIController.cs
@@ -14,8 +14,8 @@ public class AIController : MonoBehaviour
     public List<GameObject> TeleportTargetObject;
     public Animator aiAnim;
     public float walkSpeed, chaseSpeed, idleTime, minIdleTime, maxIdleTime, sightDistance, catchDistance
-        ,chaseTime, minChaseTime, maxChaseTime, screamTime, attackTime;
-
+        , chaseTime, minChaseTime, maxChaseTime, screamTime, attackTime;
+    public AudioSource audioScream, audioChase, audioBackground, audioFootstep;
     // screaming: scream 하는 상태
     // screamed: 처음 한번만 scream 애니메이션 재생, 이후 chasing으로 진입해야함
     // attack은 모든 상태에서 전환될 수 있음
@@ -25,8 +25,7 @@ public class AIController : MonoBehaviour
     public Vector3 rayCastOffset;
     private Transform currentDest;
     private Vector3 dest;
-    private bool needTeleport, needTeleport2;
-
+    private float distance;
     private void Init()
     {
         attacking = false;
@@ -34,11 +33,13 @@ public class AIController : MonoBehaviour
         screamed = false;
         chasing = false;
         walking = true;
-        needTeleport = true;
         targetDestinationIndex = 0;
         teleportTargetIndex = 0;
         currentDest = destinations[targetDestinationIndex];
-
+        audioFootstep.volume = 1.0f;
+        audioBackground.volume = 0.75f;
+        audioScream.volume = 0.75f;
+        audioChase.volume = 0.75f;
     }
     private void Start()
     {
@@ -46,21 +47,23 @@ public class AIController : MonoBehaviour
     }
     private void FixedUpdate()
     {
+        distance = Vector3.Distance(transform.position, player.transform.position);
         #region Raycast
         Vector3 direction = player.position - transform.position;
         direction = direction.normalized;
         RaycastHit hit;
         if (Physics.Raycast(transform.position + rayCastOffset, direction, out hit, sightDistance))
         {
-            if(hit.collider.gameObject.tag == "Player")
+            if (hit.collider.gameObject.tag == "Player")
             {
                 //Debug.Log("Player detected");
                 walking = false;
                 StopCoroutine("stayIdle");
                 StopCoroutine("chaseRoutine");
                 // 처음 player 적발시에만 바로 chasing 하지않고 screaming -> chasing
-                if (!screamed) 
+                if (!screamed)
                 {
+
                     StartCoroutine("scream");
                     screaming = true;
                 }
@@ -75,8 +78,15 @@ public class AIController : MonoBehaviour
 
         #region chasing
 
-        if (chasing)
+        if (chasing && distance >= catchDistance)
         {
+            if(!audioChase.isPlaying)
+            {
+                audioBackground.Stop();
+                audioChase.Play();
+            }
+            Debug.Log("chasing");
+            StopCoroutine("attack");
             dest = player.position;
             transform.LookAt(player.position);
             ai.destination = dest;
@@ -84,6 +94,7 @@ public class AIController : MonoBehaviour
             aiAnim.ResetTrigger("scream");
             aiAnim.ResetTrigger("walk");
             aiAnim.ResetTrigger("idle");
+            aiAnim.ResetTrigger("attack");
             aiAnim.SetTrigger("chase");
         }
         #endregion
@@ -91,6 +102,9 @@ public class AIController : MonoBehaviour
         #region walking
         if (walking)
         {
+            if (!audioBackground.isPlaying)
+                audioBackground.Play();
+            screamed = false;
             //Debug.Log("walking");
             dest = currentDest.position;
             ai.destination = dest;
@@ -98,6 +112,7 @@ public class AIController : MonoBehaviour
             aiAnim.ResetTrigger("scream");
             aiAnim.ResetTrigger("chase");
             aiAnim.ResetTrigger("idle");
+            aiAnim.ResetTrigger("attack");
             aiAnim.SetTrigger("walk");
             if (ai.remainingDistance <= ai.stoppingDistance)
             {
@@ -113,15 +128,17 @@ public class AIController : MonoBehaviour
         #endregion
 
         #region screaming
-        if (screaming) 
+        if (screaming)
         {
             Debug.Log("screaming");
             // 정지 상태에서 scream
+
             ai.speed = 0;
             transform.LookAt(player.position);
             aiAnim.ResetTrigger("chase");
             aiAnim.ResetTrigger("idle");
             aiAnim.ResetTrigger("walk");
+            aiAnim.ResetTrigger("attack");
             aiAnim.SetTrigger("scream");
             screaming = false;
         }
@@ -138,17 +155,20 @@ public class AIController : MonoBehaviour
             transform.LookAt(player.position);
             aiAnim.ResetTrigger("chase");
             aiAnim.ResetTrigger("idle");
+            aiAnim.ResetTrigger("attack");
             aiAnim.ResetTrigger("walk");
             aiAnim.ResetTrigger("scream");
             aiAnim.SetTrigger("attack");
+            chasing = true;
+            StopCoroutine("attack");
         }
 
         #endregion
 
         #region check hit
-
-        if (Vector3.Distance(transform.position, player.transform.position) <= catchDistance) // 가까이 붙으면 잡힌걸로 판정
+        if (distance <= catchDistance) // 가까이 붙으면 잡힌걸로 판정
         {
+            Debug.Log("catch, distance: " + distance);
             // 잡혔을때 주어진 위치로 이동 //
             chasing = false;
             StartCoroutine("attack");
@@ -158,9 +178,27 @@ public class AIController : MonoBehaviour
 
     }
 
+    // Scream Animation End frame - Animation Event
+    private void PlayScreamAudio()
+    {
+        audioScream.Play();
+    }
+    private void PlayFootstep()
+    {
+        audioFootstep.Play();
+    }
+    private void ConvertScreamToChase()
+    {
+        if (!audioChase.isPlaying)
+        {
+            audioScream.Stop();
+            audioChase.Play();
+        }
+    }
+    // Attack Animation - Animation Event에서 사용
     private void teleport()
     {
-        //Debug.Log("teleport");
+        Debug.Log("teleport");
         chasing = true;
         attacking = false;
 
@@ -182,10 +220,13 @@ public class AIController : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (targetDestinationIndex <= destinationAmount)
-            targetDestinationIndex++;
-        else
-            targetDestinationIndex = 0;
+        if (other.gameObject.tag == "Destination")
+        {
+            if (targetDestinationIndex <= destinationAmount)
+                targetDestinationIndex++;
+            else
+                targetDestinationIndex = 0;
+        }
     }
     IEnumerator scream() // scream animation -> chase animation
     {
@@ -195,11 +236,12 @@ public class AIController : MonoBehaviour
     }
     IEnumerator attack() // any state -> attack , attack -> chase
     {
+        Debug.Log("coroutine attack");
         yield return new WaitForSeconds(attackTime);
-        attacking = false;
         chasing = true;
+        attacking = false;
     }
-    IEnumerator stayIdle() 
+    IEnumerator stayIdle()
     {
         idleTime = Random.Range(minIdleTime, maxIdleTime);
         yield return new WaitForSeconds(idleTime);
@@ -212,7 +254,6 @@ public class AIController : MonoBehaviour
         yield return new WaitForSeconds(chaseTime);
         walking = true;
         chasing = false;
-
         currentDest = destinations[targetDestinationIndex];
     }
 }
