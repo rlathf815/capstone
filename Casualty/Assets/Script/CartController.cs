e936f6d048a990d040c3f2b2809d1cbd0a71496c
rlathf815 <rlathfrlathf@gmail.com>
2023-05-22 20:51:32 +0900

    집가기전커밋

--- Casualty/Assets/Script/CartController.cs
+++ Casualty/Assets/Script/CartController.cs
@@ -10,17 +10,43 @@ public class CartController : MonoBehaviour
     public GameObject cart;
     public GameObject newCart;
     public SharedData sharedData;
-    private Vector3 offset; 
+    private Vector3 offset;
+    public GameObject Alex;
+    public CanvasGroup alex;
+    public GameObject Sarah;
+    public CanvasGroup sarah;
+
 
     private void Start()
     {
         if (sharedData.dillemaPatient != 0)
+        {
+            Debug.Log("patient killed");
+            if(sharedData.dillemaPatient ==1)
+            {
+                Alex.SetActive(false);
+                StartCoroutine(subtitle(Sarah, sarah));
+            }
+            else if(sharedData.dillemaPatient ==2)
+            {
+                Sarah.SetActive(false);
+                StartCoroutine(subtitle(Alex, alex));
+            }
             cart.SetActive(true);
+        }
+            
         else
+        {
+            Debug.Log("patient not yet killed");
+            Sarah.SetActive(false);
+            Alex.SetActive(false);
             cart.SetActive(false);
+        }
+            
         offset = transform.position - player.position;
+        sharedData.bodyParked = false;
 
-       
+
         GetComponent<Collider>().enabled = false;
         newCart.SetActive(false);
         
@@ -56,5 +82,28 @@ public class CartController : MonoBehaviour
             sharedData.bodyParked = true;
         }
 
+    }
+    private IEnumerator subtitle(GameObject subtitle, CanvasGroup canvasGroup)
+    {
+        subtitle.SetActive(true);
+        while (canvasGroup.alpha < 1f)
+        {
+            canvasGroup.alpha += Time.deltaTime / 0.7f;
+            yield return null;
+        }
+        canvasGroup.alpha = 1f;
+
+        // Wait for 2 seconds
+        yield return new WaitForSeconds(2f);
+
+        // Fade out
+        while (canvasGroup.alpha > 0f)
+        {
+            canvasGroup.alpha -= Time.deltaTime / 0.7f;
+            yield return null;
+        }
+        canvasGroup.alpha = 0f;
+
+        subtitle.SetActive(false);
     }
 }
