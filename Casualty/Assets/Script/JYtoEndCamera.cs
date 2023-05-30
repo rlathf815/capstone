40af6945fd5fede82051a51a11dae3aff6c50c10
kimjoungyoo <94528532+kimjoungyoo@users.noreply.github.com>
2023-05-30 18:41:18 +0900

    ToEnding씬 추가
    +탈출하면서 페인드인
    -> 달리다 넘어짐+이명
    -> 간호사가 뭐라하는데 이명때문에 muffed
    -> 안도의한숨쉬면서 엔딩씬으로 전환

--- Casualty/Assets/Script/JYtoEndCamera.cs
+++ Casualty/Assets/Script/JYtoEndCamera.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class JYtoEndCamera : MonoBehaviour
 {
@@ -8,19 +9,27 @@ public class JYtoEndCamera : MonoBehaviour
     private Animator animator;
     public AnimationClip toRun;
     public AnimationClip toTurn;
+    public SharedData sharedData;
 
     public GameObject nurse;
     private Animator nurseAnim;
+
+    public GameObject fadeOut;
+
 
     // Start is called before the first frame update
     void Start()
     {
         nurse.SetActive(false);
+        fadeOut.SetActive(false);
 
         animator = GetComponent<Animator>();
         nurseAnim = nurse.GetComponent<Animator>();
 
         StartCoroutine(PlayAct());
+
+        sharedData.glitchOn = false;
+        
     }
 
     // Update is called once per frame
@@ -30,7 +39,7 @@ public class JYtoEndCamera : MonoBehaviour
     }
     private IEnumerator PlayAct()
     {
-        yield return new WaitForSeconds(1.0f);
+        yield return new WaitForSeconds(0.5f);
         Debug.Log("wait");
         animator.SetTrigger("toRun");
         yield return new WaitForSeconds(toRun.length);
@@ -42,7 +51,12 @@ public class JYtoEndCamera : MonoBehaviour
         nurseAnim.SetTrigger("trig");
         yield return new WaitForSeconds(toTurn.length);
         
-        animator.SetTrigger("ToBreath");
-        yield return new WaitForSeconds(1.5f);
+        animator.SetTrigger("ToBreath");        
+        yield return new WaitForSeconds(1.0f);
+        fadeOut.SetActive(true);
+        
+        yield return new WaitForSeconds(3.0f);
+        Debug.Log("scene");
+        SceneManager.LoadScene("Ending");
     }
 }
